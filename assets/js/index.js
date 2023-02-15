215392feff77830927b93ecc721c092b8ba6a79d
basodam <basodam@hva.nl>
2023-02-15 22:49:34 +0100

    directorys aangemaakt binnen assets

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
 function checkPassword() {
   var password = document.getElementById("password").value;
   if (password === "battieboys") {
-    document.getElementById("accesgranted").src = "assets/accesgranted.svg";
+    document.getElementById("accesgranted").src = "assets/img/accesgranted.svg";
     document.getElementById("accesgranted").style.display = "block";
       setTimeout(function() {
       window.location.href = "homepage.html";
     }, 1500);
   } else {
-    document.getElementById("accesdenied").src = "assets/accesdenied.svg";
+    document.getElementById("accesdenied").src = "assets/img/accesdenied.svg";
     document.getElementById("accesdenied").style.display = "block";
       setTimeout(function() {
       window.location.href = "index.html";
