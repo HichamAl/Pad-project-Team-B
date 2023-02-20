7c3be56487df6183bb7a92b4c24620636147d8a2
basodam <basodam@hva.nl>
2023-02-20 22:13:30 +0100

    hoofdpijn

---
+++ assets/js/home.js
@@ -0,0 +1,14 @@
+function redirectlvl1() {
+    window.location.href = "level1.html";
+  
+  }
+  
+  function redirectlvl2() {
+    window.location.href = "level2.html";
+  
+  }
+
+  function redirectlvl3() {
+    window.location.href = "level3.html";
+  
+  }
