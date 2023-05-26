761dd7ee3cfd2c7fc57b45d21426562a2980b398
basodam <basodam@hva.nl>
2023-05-26 17:21:48 +0200

    js files schoon

--- Django/pad/main/static/main/assets/js/index.js
+++ Django/pad/main/static/main/assets/js/index.js
@@ -1,22 +1,5 @@
 
 
-function checkPassword() {
-  var password = document.getElementById("password").value;
-  if (password === "battieboys") {
-    document.getElementById("accesgranted").src = "/static/main/assets/img/accesgranted.svg";
-    document.getElementById("accesgranted").style.display = "block";
-      setTimeout(function() {
-      window.location.href = "../enter";
-    }, 1500);
-  } else {
-    document.getElementById("accesdenied").src = "/static/main/assets/img/accesdenied.svg";
-    document.getElementById("accesdenied").style.display = "block";
-      setTimeout(function() {
-      window.location.href = "../";
-    }, 1500);
-  }
-}
-
 function redirectLogin() {
   window.location.href = "../login";
 
