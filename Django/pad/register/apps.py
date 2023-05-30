0aecff537b5141c1651f0db2284b86393854594f
Hicham <hicham.almakroudi@hva.nl>
2023-05-30 16:40:41 +0200

    comments

--- Django/pad/register/apps.py
+++ Django/pad/register/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# builds the app for the django project
 class RegisterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'register'
--- Django/pad/register/views.py
+++ Django/pad/register/views.py
@@ -7,8 +7,10 @@ from django.contrib import messages
 
 # register view 
 def register(response):
+    # if a user registers for an account the form gets submitted
     if response.method == "POST":
         form = RegisterForm(response.POST)
+        # if the form is valid it gets saved and the account is created
         if form.is_valid():
             form.save()
             messages.success(response, 'Your account has been successfully created!')
@@ -20,7 +22,9 @@ def register(response):
 
 # sign out view
 class SignedOutView(TemplateView):
+    # set the template to the right html file
     template_name = "registration/signed_out.html"
     def get(self, request: HttpRequest):
+        # if a user goes to /logout this gets activated and the user gets logged out
         logout(request)
         return render(request, self.template_name)
